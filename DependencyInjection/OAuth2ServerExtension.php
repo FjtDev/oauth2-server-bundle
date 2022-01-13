6f83e77433b2aeeef938c3a9445147260dd45581
Yann Chiousse <y.chiousse@florajet.com>
2022-01-13 15:46:34 +0100

    [ADD] SensioFrameworkextrabundle and configuration services

--- DependencyInjection/OAuth2ServerExtension.php
+++ DependencyInjection/OAuth2ServerExtension.php
@@ -2,6 +2,7 @@
 
 namespace OAuth2\ServerBundle\DependencyInjection;
 
+use Symfony\Bundle\FrameworkBundle\DependencyInjection\Configuration;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;

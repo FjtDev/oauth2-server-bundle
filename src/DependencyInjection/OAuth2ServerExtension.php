6ad85c39843caf60e49f0ac5887341e34aa631be
Yann Chiousse <y.chiousse@florajet.com>
2022-01-21 11:38:53 +0100

    [ADD] Load services.yaml in configuration

--- src/DependencyInjection/OAuth2ServerExtension.php
+++ src/DependencyInjection/OAuth2ServerExtension.php
@@ -12,11 +12,12 @@ class OAuth2ServerExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
+        $configuration = new Configuration(false);
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
+        $loader->load('services.yaml');
     }
 
     public function getAlias()
